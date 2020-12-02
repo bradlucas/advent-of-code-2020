dbbf4cd37dd6b2d637512a5bf4d3a712f6fccdfe
Brad Lucas <brad@beaconhill.com>
2020-12-01 23:05:57 -0500

    remove comment

--- test/advent/day01_test.clj
+++ test/advent/day01_test.clj
@@ -3,28 +3,12 @@
             [advent.day01 :refer [part1 part2]]))
 
 
-
-(comment
-
-  advent.day01> (part1 "day01-example.txt")
-  514579
-  advent.day01> (part1 "day01.txt")
-  1005459
-  advent.day01> (part2 "day01-example.txt")
-  241861950
-  advent.day01> (part2 "day01.txt")
-  92643264
-
-  )
-
-
 (deftest part1-test
   (let [expected 514579]
     (is (= expected (part1 "day01-example.txt"))))
 
   (let [expected 1005459]
     (is (= expected (part1 "day01.txt")))))
-
 
 
 (deftest part2-test
