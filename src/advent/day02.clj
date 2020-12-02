2dfb7868c9be7699e68c99a927873dcc185047de
Brad Lucas <brad@beaconhill.com>
2020-12-02 09:00:29 -0500

    part2 day2

--- src/advent/day02.clj
+++ src/advent/day02.clj
@@ -22,3 +22,23 @@
 (defn part1 [file]
   (let [lst (read-input file)]
     (count (filter true? (map parse-input lst)))))
+
+
+
+
+(defn parse-input2 [input]
+  (let [[v c s] (str/split input #" ")
+        [p1 p2]  (map #(Integer/parseInt %) (str/split v #"-"))
+        chr (first (first (str/split c #":")))
+        c1 (get s (dec p1))
+        c2 (get s (dec p2))]
+    (if (not (= c1 c2))
+      (and (or (= c1 chr) (= c2 chr))))))
+
+
+(defn part2 [file]
+  ;; a-b are positions
+  ;; one of which must contain the letter
+  ;; valid if one or the other position has the value
+  (let [lst (read-input file)]
+    (count (filter true? (map parse-input2 lst)))))
