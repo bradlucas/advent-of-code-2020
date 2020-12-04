796966d6a21e91d68ad52bc2ddbc932cf6c5a935
Brad Lucas <brad@beaconhill.com>
2020-12-03 23:06:20 -0500

    day3 part2

--- src/advent/day03.clj
+++ src/advent/day03.clj
@@ -30,3 +30,33 @@
         (if (> row rows)
           (count (filter #(= \# %) path))
           (recur (step pos) (conj path (get grid pos))))))))
+
+
+
+;; Part2
+;; Run for a number of slopes and multiple all the results
+
+(def slopes [{:right 1 :down 1}
+             {:right 3 :down 1}
+             {:right 5 :down 1}
+             {:right 7 :down 1}
+             {:right 1 :down 2}])
+
+(defn step2 [{:keys [right down] :as slope} {:keys [col row] :as pos}]
+  (let [next {:col (+ col (:right slope))
+              :row (+ row (:down slope))}]
+    next))
+
+(defn traverse [grid slope]
+  (let [rows (dec (count grid))]
+    (loop [pos {:col 0 :row 0}
+           path []]
+      (let [row (:row pos)]
+        (if (> row rows)
+          (count (filter #(= \# %) path))
+          (recur (step2 slope pos) (conj path (get grid pos))))))))
+
+
+(defn part2 [file]
+  (let [grid (read-input file)]
+    (apply * (map #(traverse grid %) slopes))))
--- test/advent/day03_test.clj
+++ test/advent/day03_test.clj
@@ -1,6 +1,6 @@
 (ns advent.day03-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day03 :refer [part1]]))
+            [advent.day03 :refer [part1 part2]]))
 
 
 (deftest part1-test
@@ -9,3 +9,11 @@
 
   (let [expected 162]
     (is (= expected (part1 "day03.txt")))))
+
+
+(deftest part2-test
+  (let [expected 336]
+    (is (= expected (part2 "day03-example.txt"))))
+
+  (let [expected 3064612320]
+    (is (= expected (part2 "day03.txt")))))
