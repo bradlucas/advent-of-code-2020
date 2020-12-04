5f59060ab349810864400c9a6a6a62e90653b50e
Brad Lucas <brad@beaconhill.com>
2020-12-04 09:51:49 -0500

    day4 part2

--- src/advent/day04.clj
+++ src/advent/day04.clj
@@ -59,3 +59,84 @@
       read-input
       (filter #(valid %))
       count))
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; Part2
+
+(defn all-digits [cnt s]
+  (let [lst (seq s)]
+    (if (= cnt (count lst))
+      (every? true? (map #(Character/isDigit %) lst)))))
+
+(defn valid-year [s n l h]
+  (and (all-digits n s)
+       (let [n (Integer/parseInt s)]
+         (and (>= n l)
+              (<= n h)))))
+
+(defn valid-birth-year [s]
+  ;; Four digits at least 1920 and at most 2002
+  (valid-year s 4 1920 2002))
+
+(defn valid-issue-year [s]
+  ;; Four digits at least 2010 and at most 2020
+  (valid-year s 4 2010 2020))
+
+(defn valid-expiration-year [s]
+  ;; four digits at least 2020 and at most 2030
+  (valid-year s 4 2002 2030))
+
+(defn valid-height [s]
+  ;; A number followed by either cm or in:
+  ;;  If cm, the number must be at least 150 and at most 193
+  ;;  If in, the number must be at least 59 and at most 76
+  ;; Can have a missing cm/in which makes the value invalid as well
+  (let [len (count s)
+        [n t] (map #(apply str %) (split-at (- len 2) (seq s)))]
+    (if (or (= t "cm") (= t "in"))
+      (let [num (Integer/parseInt n)]
+        (if (= t "cm")
+          (and (>= num 150)
+               (<= num 193))
+          (if (= t "in")
+            (and (>= num 59)
+                 (<= num 76))))))))
+
+(defn valid-hair-color [s]
+  ;; A # followed by exactly six characters 0-9 or a-f
+  (and (= \# (get s 0))
+       (= 7 (count s))
+       (= 6 (count (re-seq #"[a-z0-9]" (apply str (rest s)))))))
+
+(defn valid-eye-color [s]
+  ;; Exactly one of: amb blu brn gry grn hzl oth
+  (let [k (keyword s)
+        colors #{:amb :blu :brn :gry :grn :hzl :oth}]
+    (colors k)))
+
+(defn num-digits [s]
+  (count (map true? (map #(Character/isDigit %) (seq s)))))
+
+(defn valid-password [s]
+  ;; A nine-digit number, including leading zeroes
+  (= 9 (num-digits s)))
+
+
+(defn extended-valid [m]
+  (and (valid m)
+       (valid-birth-year (:byr m))
+       (valid-issue-year (:iyr m))
+       (valid-expiration-year (:eyr m))
+       (valid-height (:hgt m))
+       (valid-hair-color (:hcl m))
+       (valid-eye-color (:ecl m))
+       (valid-password (:pid m))))
+
+
+(defn part2 [file]
+  (->> file
+       read-input
+       (filter #(extended-valid %))
+       count))
--- test/advent/day04_test.clj
+++ test/advent/day04_test.clj
@@ -1,6 +1,6 @@
 (ns advent.day04-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day04 :refer [part1]]))
+            [advent.day04 :refer [part1 part2]]))
 
 
 (deftest part1-test
@@ -8,5 +8,12 @@
     (is (= expected (part1 "day04-example.txt"))))
 
   (let [expected 260]
-    (is (= expected (part1 "day04.txt"))))
-  )
+    (is (= expected (part1 "day04.txt")))))
+
+
+(deftest part2-test
+  (let [expected 4]
+    (is (= expected (part2 "day04-2-example.txt"))))
+
+  (let [expected 153]
+    (is (= expected (part2 "day04.txt")))))
