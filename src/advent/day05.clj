b68e1abd2069835d1d8bb3825d5cb2c5e6f2c6c4
Brad Lucas <brad@beaconhill.com>
2020-12-05 08:40:15 -0500

    day5 part 1

---
+++ src/advent/day05.clj
@@ -0,0 +1,54 @@
+(ns advent.day05
+  (:require [advent.core :as c]
+            [clojure.java.io :as io]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]))
+
+;; Total number of rows is 128
+
+(def rows 128)
+(def cols 8)
+
+;; First 7 characters are either F or B
+;; The last three characters will be either L or R
+
+
+;; split-at 7
+(defn read-input [file]
+  (->> file
+       (c/read-lines)
+       (map #(split-at 7 %))))
+
+;; FBFBBFFRLR
+;; 44
+;; (range 128)
+;; \F  -- left-half
+;; \B -- right-half
+
+(defn lower-half [col]
+  ;; split in the middle and return the left half
+  (subvec col 0 (/ (count col) 2)))
+
+(defn upper-half [col]
+  ;; split in the middle and return the right half
+  (subvec col (/ (count col) 2) (count col)))
+
+
+(defn half [col c]
+  (if (or (= \F c) (= \L c))
+    (lower-half col)
+    (upper-half col)))
+
+(defn row [s]
+  (first (reduce half (vec (range rows)) s)))
+
+(defn col [s]
+  (first (reduce half (vec (range cols)) s)))
+
+(defn seat [row col]
+  (+ (* row 8) col))
+
+(defn part1 [file]
+  (->> (read-input file)
+       (map (fn [[a b]] (seat (row a) (col b))))
+       (apply max)))
---
+++ test/advent/day05_test.clj
@@ -0,0 +1,20 @@
+(ns advent.day05-test
+  (:require [clojure.test :refer [deftest is]]
+            [advent.day05 :refer [part1]]))
+
+
+(deftest part1-test
+  (let [expected 820]
+    (is (= expected (part1 "day05-example.txt"))))
+
+  ;; (let [expected 0]
+  ;;   (is (= expected (part1 "day05.txt"))))
+  )
+
+
+;; (deftest part2-test
+;;   (let [expected 0]
+;;     (is (= expected (part1 "day05-example.txt"))))
+
+;;   (let [expected 0]
+;;     (is (= expected (part1 "day05.txt")))))
