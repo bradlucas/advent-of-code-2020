2c98efd94463220ea491714fb743827e7fda3035
Brad Lucas <brad@beaconhill.com>
2020-12-11 11:15:06 -0500

    day 10 part 2

--- src/advent/day10.clj
+++ src/advent/day10.clj
@@ -10,3 +10,31 @@
 (defn part1 [file]
   (let [lst (read-input file)]
     (apply * (vals (frequencies (map #(- %2 %1) lst (rest lst)))))))
+
+
+
+;; Part2
+;; Number of paths where the difference isn't more than 3
+;; You can drop adapters if needed
+
+;; @see https://github.com/benfle/advent-of-code-2020/blob/main/day10.clj
+;; reverse the input
+;; working from the beginning of the list (the tail)
+;; for each node calc the number of paths possible to the next node
+;; by looking for the existance of values idx + 1, idx + 2, idx +3
+;; return the number found for the last node (the original first node
+
+(defn count-next [acc n]
+  (assoc acc n (+ (get acc (+ n 1) 0)
+                  (get acc (+ n 2) 0)
+                  (get acc (+ n 3) 0))))
+
+(defn part2 [file]
+  (let [lst (sort (read-input file))]
+    (loop [acc {(last lst) 1}
+           lst (rest (reverse lst))]
+      (if (empty? lst)
+        (acc 0)
+        (recur (count-next acc (first lst)) (rest lst))))))
+
+
--- test/advent/day10_test.clj
+++ test/advent/day10_test.clj
@@ -1,6 +1,6 @@
 (ns advent.day10-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day10 :refer [part1]]))
+            [advent.day10 :refer [part1 part2]]))
 
 
 (deftest part1-test
@@ -11,11 +11,12 @@
      (is (= expected (part1 "day10.txt"))))
   )
 
+(deftest part2-test
+  (let [expected 8]
+    (is (= expected (part2 "day10-example0.txt"))))
 
-;; (deftest part2-test
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day10-example.txt" (part1 "day10-example.txt)))))
+  (let [expected 19208]
+    (is (= expected (part2 "day10-example.txt"))))
 
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day10.txt" (part1 "day10.txt")))))
-;;  )
+  (let [expected 3100448333024]
+    (is (= expected (part2 "day10.txt")))))
