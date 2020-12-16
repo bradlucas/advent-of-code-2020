017a3818ced644d8a8f8935d63141a2fdfc174cd
Brad Lucas <brad@beaconhill.com>
2020-12-16 10:56:23 -0500

    day 13 part 2

--- test/advent/day13_test.clj
+++ test/advent/day13_test.clj
@@ -1,6 +1,7 @@
 (ns advent.day13-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day13 :refer [part1]]))
+            [advent.day13 :refer [part1]]
+            [advent.day13-2 :refer [part2]]))
 
 
 (deftest part1-test
@@ -10,9 +11,9 @@
   (let [expected 3606]
      (is (= expected (part1 "day13.txt")))))
 
-;; (deftest part2-test
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day13-example.txt"))))
+(deftest part2-test
+  (let [expected 1068781]
+    (is (= expected (part2 "day13-example.txt"))))
 
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day13.txt")))))
+  (let [expected 379786358533423]
+    (is (= expected (part2 "day13.txt")))))
