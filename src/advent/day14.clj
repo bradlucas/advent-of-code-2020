4afe758c8db996300578d382ffaa3097fa954acc
Brad Lucas <brad@beaconhill.com>
2020-12-17 08:52:04 -0500

    day 14 part 2

--- src/advent/day14.clj
+++ src/advent/day14.clj
@@ -62,3 +62,78 @@
 (defn part1 [file]
   (add-values (vals (process (c/read-lines file)))))
 
+
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; Part2
+
+(defn parse-line2 [s]
+  (let [[c v] (-> s
+                  (str/split #" = "))]
+    (if ( = c "mask")
+      {:cmd :mask
+       :val v}
+      (let [[_ a] (-> c
+                      (str/split #"[\[\]]"))]
+        {:cmd :mem
+         :addr (left-pad 36 (Integer/toBinaryString (Integer/parseInt a)))
+         :val (Integer/parseInt v)}))))
+
+
+;; @see https://github.com/bnii/advent-of-code-2020/blob/main/src/day14.clj
+;; Added (apply str s) to return values as binary strings
+(defn floating->concretes [floating-location]
+  (loop [current-locations [[]]
+         [actual-bit & remainder-bits] floating-location]
+    (cond
+      (nil? actual-bit) (map (fn [s] (apply str s))current-locations)
+      (#{\0 \1} actual-bit) (recur (map #(conj % actual-bit) current-locations) remainder-bits)
+      :else (recur (concat
+                     (map #(conj % \1) current-locations)
+                     (map #(conj % \0) current-locations))
+                   remainder-bits))))
+
+
+(defn mask-addr [mask val]
+  (let [result (apply str (map   (fn [a b] (if (= \0 a)
+                           b
+                           (if (= \1 a)
+                             \1
+                             \X)))
+                            mask val))]
+    ;; Purmutations of result replacing all Xs with 0 or 1
+    (floating->concretes result)))
+
+(defn process-value2 [m acc i]
+  (let [addr (:addr i)
+        val (:val i)]
+    ;; mask the addr
+    (let [addresses (mask-addr m addr)]
+      ;; load the addresses with the val
+      (loop [a addresses
+             acc acc]
+        (if (empty? a)
+          [m acc]
+          (recur (rest a) (assoc acc (first a) val)))))))
+
+(defn process-line [m acc i]
+  (if (= :mask (:cmd i))
+    [(:val i) acc]
+    (process-value2 m acc i)))
+
+
+(defn part2 [file]
+  (let [instructions (map parse-line2 (c/read-lines file))]
+    (loop [i instructions
+           mask nil
+           acc {}]
+      (if (empty? i)
+        (apply + (vals acc))
+        (let [[m a] (process-line mask acc (first i))]
+          (recur (rest i) m a))))))
+
+
+
+
--- test/advent/day14_test.clj
+++ test/advent/day14_test.clj
@@ -1,6 +1,6 @@
 (ns advent.day14-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day14 :refer [part1]]))
+            [advent.day14 :refer [part1 part2]]))
 
 
 (deftest part1-test
@@ -10,9 +10,9 @@
   (let [expected 3059488894985]
      (is (= expected (part1 "day14.txt")))))
 
-;; (deftest part2-test
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day14-example.txt"))))
+(deftest part2-test
+  (let [expected 208]
+    (is (= expected (part2 "day14-example2.txt"))))
 
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day14.txt")))))
+  (let [expected 2900994392308]
+    (is (= expected (part2 "day14.txt")))))
