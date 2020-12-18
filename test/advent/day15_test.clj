3575c85ef74b37b3eae77cb35d9e07104b584ca1
Brad Lucas <brad@beaconhill.com>
2020-12-18 08:25:58 -0500

    day 15 part 2

--- test/advent/day15_test.clj
+++ test/advent/day15_test.clj
@@ -1,16 +1,15 @@
 (ns advent.day15-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day15 :refer [part1]]))
+            [advent.day15-2 :refer [part1 part2]]))
 
 
 (deftest part1-test
   (let [expected 436]
     (is (= expected (part1 [0,3,6]))))
 
-    (let [expected 0]
+    (let [expected 852]
       (is (= expected (part1 [0,3,1,6,7,5])))))
 
-
-;; (deftest part2-test
-;;   (let [expected 0]
-;;     (is (= expected (part2)))))
+(deftest part2-test
+  (let [expected 6007666]
+    (is (= expected (part2)))))
