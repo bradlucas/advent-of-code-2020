188debb7d0e84559e53efddf782056b313ca8f0a
Brad Lucas <brad@beaconhill.com>
2020-12-25 21:21:59 -0500

    day 17 part 2

--- src/advent/day17.clj
+++ src/advent/day17.clj
@@ -8,9 +8,11 @@
 ;; https://github.com/euccastro/advent-of-code-2020/blob/master/src/advent/day17.clj
 ;; https://github.com/zelark/AoC-2020/blob/master/src/zelark/aoc_2020/day_17.clj
 
+
 (def example ".#.
 ..#
 ###")
+
 
 ;; euccastor
 (defn initial-state [input]
@@ -24,6 +26,8 @@
 (defn parse-input [input n]
   (->> (str/split-lines input)
        (mapcat (fn [y line] (keep-indexed (fn [x ch] (when (= ch \#) [x y])) line)) (range))
+       ;; [1 0] => [0]
+       ;; [1 0] => [0 0]
        (map #(into % (repeat n 0)))
        set))
 
@@ -39,9 +43,11 @@
 
 ;; ----------------------------------------------------------------------------------------------------
 
-(defn read-input [file]
+(defn read-input [file n]
   (-> (c/read-input file)
-      (initial-state)))
+      ;; (initial-state)
+      (parse-input n)
+      ))
 
 ;; (range -1 2)
 ;; (-1 0 1)
@@ -61,7 +67,7 @@
 
 
 (defn part1 [file]
-  (->> (read-input file)
+  (->> (read-input file 1)
        (iterate (partial step neighbors))
        (drop 6)
        (first)
@@ -69,5 +75,39 @@
        ))
 
 
-;; (part1 "day17-example.txt) => 112
-;; (part1 "day17.txt" => 338
+
+
+;; advent.day17> (part1 "day17-example.txt")
+;; 112
+;; advent.day17> (part1 "day17.txt")
+;; 338
+
+
+
+(defn neighbors-4d [[x y z w]]
+  (for [dx (range -1 2)
+        dy (range -1 2)
+        dz (range -1 2)
+        dw (range -1 2)
+        :when (not= 0 dx dy dz dw)]
+    [(+ x dx)
+     (+ y dy)
+     (+ z dz)
+     (+ w dw)]))
+
+
+(defn part2 [file]
+  (->> (read-input file 2)
+       (iterate (partial step neighbors-4d))
+       (drop 6)
+       (first)
+       (count)
+       ))
+
+
+
+
+;; advent.day17> (part2 "day17-example.txt")
+;; 848
+;; advent.day17> (part2 "day17.txt")
+;; 2440
--- test/advent/day17_test.clj
+++ test/advent/day17_test.clj
@@ -1,6 +1,6 @@
 (ns advent.day17-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day17 :refer [part1]]))
+            [advent.day17 :refer [part1 part2]]))
 
 
 (deftest part1-test
@@ -10,9 +10,9 @@
   (let [expected  338]
     (is (= expected (part1 "day17.txt")))))
 
-;; (deftest part2-test
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day17-example.txt"))))
+(deftest part2-test
+  (let [expected 848]
+    (is (= expected (part2 "day17-example.txt"))))
 
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day17.txt")))))
+  (let [expected 2440]
+    (is (= expected (part2 "day17.txt")))))
