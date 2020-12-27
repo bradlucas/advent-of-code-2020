d8de4c3d66b6665f374b33284f951994febe4150
Brad Lucas <brad@beaconhill.com>
2020-12-27 10:17:03 -0500

    day18 part 2

--- src/advent/day18.clj
+++ src/advent/day18.clj
@@ -50,3 +50,48 @@
 
 
 ;; (part1 "day18.txt") => 24650385570008
+
+
+
+;; ----------------------------------------------------------------------------------------------------
+;; part2
+
+;; addition is evaluated before multiplication
+
+
+
+(defn eval2 [tokens]
+  (letfn [(element [[token & tokens]]
+            (case token
+              :open (let [[a [_close & tokens]] (mult tokens)]
+                      [a tokens])
+              [token tokens]))
+
+          (mult [tokens]
+            (let [[a [op & tokens] :as left] (add tokens)]
+              (case op
+                :times (let [[ b tokens] (mult tokens)]
+                         [(* a b) tokens])
+                left)))
+
+          (add [tokens]
+            (let [[a [op & tokens] :as left] (element tokens)]
+              (case op
+                :plus (let [[ b tokens] (add tokens)]
+                        [(+ a b) tokens])
+                left)))]
+    (first (mult tokens))))
+
+
+;; (eval2 (parse example)) => 231
+;; (eval2 (parse example2)) => 51
+
+
+(defn part2 [file]
+  (->> (c/read-lines file)
+       (map parse)
+       (map eval2)
+       (reduce +)))
+
+
+;; (part2 "day18.txt") =>
--- test/advent/day18_test.clj
+++ test/advent/day18_test.clj
@@ -1,6 +1,6 @@
 (ns advent.day18-test
   (:require [clojure.test :refer [deftest is]]
-            [advent.day18 :refer [part1]]))
+            [advent.day18 :refer [part1 part2]]))
 
 
 (deftest part1-test
@@ -10,9 +10,9 @@
   (let [expected 24650385570008]
     (is (= expected (part1 "day18.txt")))))
 
-;; (deftest part2-test
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day18-example.txt"))))
+(deftest part2-test
+  (let [expected 23340]
+    (is (= expected (part2 "day18-example.txt"))))
 
-;;   (let [expected 0]
-;;     (is (= expected (part2 "day18.txt")))))
+  (let [expected 158183007916215]
+    (is (= expected (part2 "day18.txt")))))
